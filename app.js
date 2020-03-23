02dcc3b6a3941f9b2fceb88e705b78592225586a
Conor Walsh <conorwalsh0703@gmail.com>
2020-03-23 17:56:16 +0000

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'block'
+
+    setTimeout(calculateResults, 500);
+
+    e.preventDefault()
+})
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
     // UI variables
     const amount = document.getElementById('amount')
     const interest = document.getElementById('interest')
     const years = document.getElementById('years')
+
     const monthlyPayment = document.getElementById('monthly-payment')
     const totalPayment = document.getElementById('total-payment')
     const totalInterest = document.getElementById('total-interest')
@@ -21,13 +30,19 @@ function calculateResults(e) {
         monthlyPayment.value = '€' + monthly.toFixed(2)
         totalPayment.value = '€' + (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2)
+        document.getElementById('results').style.display = 'block'
+        document.getElementById('loading').style.display = 'none'
     } else {
         showError('Please check your numbers')
+
     }
-    e.preventDefault()
+
+
 }
 
 function showError(message) {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'none';
     const errorDiv = document.createElement('div')
     const card = document.querySelector('.card')
     const heading = document.querySelector('.heading')
@@ -36,6 +51,7 @@ function showError(message) {
     errorDiv.appendChild(document.createTextNode(message))
 
     card.insertBefore(errorDiv, heading)
+
 
     setTimeout(clearError, 3000)
 }
